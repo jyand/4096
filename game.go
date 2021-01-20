95acf65031436cb7e7e98033270cf9f7bcb06e40
jyand <jyand@protonmail.com>
2021-01-20 05:04:22 -0500

    preliminary stuff

--- game.go
+++ game.go
@@ -1,10 +1,54 @@
 package main
 import (
         "fmt"
-        "os"
-        "bufio"
+        //"os"
+        //"bufio"
         "math"
 )
 
+var uint8 N = 4
+
+func PlaceRandom(squares [N][N]uint16) {
+        rand.Seed(time.Now().UnixNano())
+        ri := rand.Intn(N)
+        rand.Seed(time.Now().UnixNano())
+        rj := rand.Intn(N)
+        if squares[ri][rj] != 0 {
+                PlaceRandom()
+        }
+        squares[ri][rj] = 2
+}
+
+func InitGame(uint8 N) [N][N]uint16{
+        var squares [N][N] uint16
+        for j := 0 ; j < N ; j++ {
+                for i := 0 ; i < N ; i++ {
+                        squares[i][j] = 0
+                }
+        }
+        PlaceRandom()
+        PlaceRandom()
+        return squares
+}
+
+// transforms the game's state each turn
+func Move(prev [N][N]uint16, k uint8, n uint8) [N][N]uint16 {
+}
+
+//polymorphic wrapper for Move()
+func Directional(s string) [N][N]uint16 {
+        switch s {
+        default: return Move()
+        case "j": return Move()
+        case "k": return Move()
+        case "l": return Move()
+        }
+}
+
 func main() {
+
+        var n float64 = 4
+        if math.Exp2(n) == math.Pow(2, n) {
+                fmt.Println("ya")
+        }
 }
